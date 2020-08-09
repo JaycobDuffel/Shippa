a892e808340e8ff1cabeef0268cb822f34be3b5c
Aubrey Blair-Pattison <aubrey.blairpattison@gmail.com>
2020-08-09 17:25:17 -0600

    styling improved

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -7,7 +7,7 @@ export default function HomePage() {
         <img src="https://images2.imgbox.com/0a/e1/LUtmV1nx_o.png" alt="header-picture" border="0" width="100%"/>
       </div>
 
-      <div className="values" style={{ display: 'flex', flexDirection: "row"}}>
+      <div className="values">
         <div  >
           <img src="https://images2.imgbox.com/b3/3b/ocjJBxC5_o.png" alt="image host" width="75" height="75" />
           <h1 style={{ color: "#5c4863", alignItems: 'center', margin: '0px 50px 0px 0px'}}> Transparency </h1>
@@ -21,7 +21,6 @@ export default function HomePage() {
           <h1 style={{ color: "#5c4863", alignItems: 'center' , margin: '0px 50px 0px 0px'}}> Simplicity </h1>
         </div>
       </div>
-
     </div>
   )
 }
